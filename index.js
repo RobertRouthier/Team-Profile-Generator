968e56a9264f0bda95f4cfaaf51e8a5e883e3a32
Robert Routhier <robrouth92@gmail.com>
2022-05-25 10:27:42 -0700

    bootstrap installed

--- index.js
+++ index.js
@@ -171,8 +171,8 @@ function createCard(employee){
             <h3 class="card-head">${employee.name}</h3>
             <h4 class="card-head">${employee.getRole()}</h4>
 
-            <p class="card-item">${employee.id}</p>
-            <p class="card-item">${employee.email}</p>
+            <p class="card-item">Employee ID:${employee.id}</p>
+            <p class="card-item">Employee Email:${employee.email}</p>
             <p class="card-item">${employee.getExtra()}</p>
         </div>
     `
@@ -186,6 +186,8 @@ function createHtml(){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Cards</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="./style.css">
     </head>
     <body>
